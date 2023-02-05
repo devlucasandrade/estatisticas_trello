f76b8489350b5cef4d3364fddfce1b31877c4812
Lucas Andrade <devlucasandrade@gmail.com>
2023-02-04 22:52:15 -0300

    chumbando as estatisticas do board

--- lib/src/app/views/list/bloc/bloc_list.dart
+++ lib/src/app/views/list/bloc/bloc_list.dart
@@ -10,10 +10,17 @@ class BlocList {
     int digital = 0;
     int televisao = 0;
     int radio = 0;
+    int espontanea = 0;
+    int respostaImprenssa = 0;
+    int artigo = 0;
+    int positiva = 0;
+    int neutra = 0;
+    int negativa = 0;
 
     for (int i = 0; i < response.length; i++) {
       final card = await cardRepository.getCard(response[i].id);
       for (int i = 0; i < card.labels!.length; i++) {
+        //Tipo de Veículo
         if (card.labels?[i].id == '63d32a592f36f2348afdde93') {
           impressa += 1;
         }
@@ -26,6 +33,26 @@ class BlocList {
         if (card.labels?[i].id == '63d32a592f36f2348afdde9f') {
           radio += 1;
         }
+        // Tipo de Mídia
+        if (card.labels?[i].id == '63d32a592f36f2348afdde9c') {
+          espontanea += 1;
+        }
+        if (card.labels?[i].id == '63d8281406cd8985df9d9dc8') {
+          respostaImprenssa += 1;
+        }
+        if (card.labels?[i].id == '63d32a592f36f2348afdde9e') {
+          artigo += 1;
+        }
+        // Classificação
+        if (card.labels?[i].id == '63d32a592f36f2348afdde9c') {
+          positiva += 1;
+        }
+        if (card.labels?[i].id == '63d8281406cd8985df9d9dc8') {
+          neutra += 1;
+        }
+        if (card.labels?[i].id == '63d32a592f36f2348afdde9e') {
+          negativa += 1;
+        }
       }
     }
     return LabelsInt(
@@ -33,6 +60,12 @@ class BlocList {
       digital: digital,
       televisao: televisao,
       radio: radio,
+      espontanea: espontanea,
+      respostaImprenssa: respostaImprenssa,
+      artigo: artigo,
+      positiva: positiva,
+      neutra: neutra,
+      negativa: negativa,
     );
   }
 }
@@ -42,11 +75,23 @@ class LabelsInt {
   int digital;
   int televisao;
   int radio;
+  int espontanea;
+  int respostaImprenssa;
+  int artigo;
+  int positiva;
+  int neutra;
+  int negativa;
 
   LabelsInt({
     required this.impressa,
     required this.digital,
     required this.televisao,
     required this.radio,
+    required this.espontanea,
+    required this.respostaImprenssa,
+    required this.artigo,
+    required this.positiva,
+    required this.neutra,
+    required this.negativa,
   });
 }
--- lib/src/app/views/list/pages/list_page.dart
+++ lib/src/app/views/list/pages/list_page.dart
@@ -20,9 +20,9 @@ class _ListPageState extends State<ListPage> {
       ),
       body: SafeArea(
         child: Container(
-          padding: const EdgeInsets.all(20),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           width: MediaQuery.of(context).size.width,
-          height: 200,
+          height: MediaQuery.of(context).size.height * 0.3,
           child: FutureBuilder<LabelsInt>(
             future: BlocList().getTotalLabels(),
             builder: (context, snapshot) {
@@ -33,15 +33,117 @@ class _ListPageState extends State<ListPage> {
                 );
               } else {
                 return Container(
-                  color: Colors.blue[200],
-                  padding: const EdgeInsets.all(20),
-                  // width: MediaQuery.of(context).size.width * 0.8,
+                  decoration: BoxDecoration(
+                    color: Colors.blue[300],
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
                   child: Column(
                     children: [
-                      Text('Impressa: ${label?.impressa}'),
-                      Text('Digital: ${label?.digital}'),
-                      Text('Televisão: ${label?.televisao}'),
-                      Text('Rádio: ${label?.radio}'),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'OCORRÊNCIAS',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue[200],
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        child: Expanded(
+                          flex: 1,
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  const Text('Impressa:'),
+                                  Text('${label?.impressa}'),
+                                  const SizedBox(width: 10),
+                                  const Text('Digital:'),
+                                  Text('${label?.digital}'),
+                                ],
+                              ),
+                              const SizedBox(height: 15),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  const Text('Televisão:'),
+                                  Text('${label?.televisao}'),
+                                  const SizedBox(width: 10),
+                                  const Text('Rádio:'),
+                                  Text('${label?.radio}'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue[200],
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Expanded(
+                          flex: 1,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              const Text('Resposta:'),
+                              Text('${label?.respostaImprenssa}'),
+                              const SizedBox(width: 10),
+                              const Text('Espontânea:'),
+                              Text('${label?.espontanea}'),
+                              const SizedBox(width: 10),
+                              const Text('Artigo:'),
+                              Text('${label?.artigo}'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue[200],
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Expanded(
+                          flex: 1,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              const Text('Positivo:'),
+                              Text('${label?.positiva}'),
+                              const SizedBox(width: 10),
+                              const Text('Neutra:'),
+                              Text('${label?.neutra}'),
+                              const SizedBox(width: 10),
+                              const Text('Negativa:'),
+                              Text('${label?.negativa}'),
+                            ],
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 );
